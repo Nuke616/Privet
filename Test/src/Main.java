54beaf0570b8dba11f04184d0059139b9ffc3f6b
Никита <nuke581@gmain.com>
2023-10-11 16:31:24 +0300

    Циклы

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    System.out.println("введи число капитан");
-    int a = new Scanner(System.in).nextInt();
-    if(a == 5) {
-        System.out.println("5");
-    } else if (a == 6) {
-        System.out.println("89");
-    } else {
-        System.out.println("ПИЗДЦ");
-    }
 
+    for(int i = 6;i >= 6; i ++) {
+        if (i == 698279)
+            break;
+        else {
+            System.out.println(i);
+        }
+    }
     }
 }
